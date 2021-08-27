e91a1488c8fe4b37491ac2e74cf27c2de463c2ae
kinjo <kinchannibo@yahoo.co.jp>
2021-08-27 18:49:22 +0900

    We correspond 'nenetinomannge'

--- a.cpp
+++ a.cpp
@@ -4,15 +4,13 @@ using namespace std;
 #define For(i, a, b) for (int i = (a); i < (b); ++i)
 #define rep(i, n) For(i, 0, n)
 //Write From this Line
+map<string, string> mp;
+void makemp();
 int main()
 {
-	string alp, hira;
-	map<string,string> mp; // alp -> hira
-	while(cin >> alp >> hira){
-		mp[alp] = hira;
-	}
+	makemp();
 	string text;
-	text = "ootukahuutahakusai";
+	cin >> text;
 	int n = text.size(); 
 	string now = "";
 	string ans = "";
@@ -28,4 +26,78 @@ int main()
 		}
 	}
 	cout << ans << endl;
+}
+
+void makemp(){
+mp["a"] = "あ";
+mp["i"] = "い";
+mp["u"] = "う";
+mp["e"] = "え";
+mp["o"] = "お";
+mp["ka"] = "か";
+mp["ki"] = "き";
+mp["ku"] = "く";
+mp["ke"] = "け";
+mp["ko"] = "こ";
+mp["sa"] = "さ";
+mp["si"] = "し"; 
+mp["su"] = "す";
+mp["se"] = "せ";
+mp["so"] = "そ";
+mp["ta"] = "た";
+mp["ti"] = "ち";
+mp["tu"] = "つ";
+mp["te"] = "て";
+mp["to"] = "と";
+mp["na"] = "な";
+mp["ni"] = "に";
+mp["nu"] = "ぬ";
+mp["ne"] = "ね";
+mp["no"] = "の";
+mp["ha"] = "は";
+mp["hi"] = "ひ";
+mp["hu"] = "ふ";
+mp["he"] = "へ";
+mp["ho"] = "ほ";
+mp["ma"] = "ま";
+mp["mi"] = "み";
+mp["mu"] = "む";
+mp["me"] = "め"; 
+mp["mo"] = "も";
+mp["ya"] = "や";
+mp["yi"] = "い";
+mp["yu"] = "ゆ";
+mp["ye"] = "いぇ";
+mp["yo"] = "よ";
+mp["ra"] = "ら";
+mp["ri"] = "り";
+mp["ru"] = "る";
+mp["re"] = "れ";
+mp["ro"] = "ろ";
+mp["wa"] = "わ";
+mp["wi"] = "うぃ";
+mp["wu"] = "う";
+mp["we"] = "うぇ";
+mp["wo"] = "を";
+mp["nn"] = "ん";
+mp["pa"] = "ぱ";
+mp["pi"] = "ぴ";
+mp["pu"] = "ぷ";
+mp["pe"] = "ぺ";
+mp["po"] = "ぽ";
+mp["ba"] = "ば";
+mp["bi"] = "び";
+mp["bu"] = "ぶ";
+mp["be"] = "べ";
+mp["bo"] = "ぼ";
+mp["da"] = "だ";
+mp["di"] = "ぢ";
+mp["du"] = "づ";
+mp["de"] = "で";
+mp["do"] = "ど";
+mp["ga"] = "が";
+mp["gi"] = "ぎ";
+mp["gu"] = "ぐ";
+mp["ge"] = "げ";
+mp["go"] = "ご";
 }
